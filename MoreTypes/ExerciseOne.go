3af372bffd5debc0741c1e023b8549307fdf0b23
juanes <juannomesque12@gmail.com>
2026-01-23 08:37:26 -0500

    feat: implement complex types using slices, maps, and pointer receivers

--- MoreTypes/ExerciseOne.go
+++ MoreTypes/ExerciseOne.go
@@ -9,32 +9,42 @@ type Producto struct {
 	Descuento float64
 }
 
+// Usamos punteros para modificar el producto real
 func (p *Producto) AplicarDescuentoReal() {
 	p.Precio = p.Precio * (1 - p.Descuento)
 }
 
-func (p *Producto) ActualizarPrecio(nuevoPrecio float64) {
-	p.Precio = nuevoPrecio
-}
-
 func main() {
+	// 1. Lista de productos
 	inventario := []Producto{
 		{Nombre: "iPhone 15", Cantidad: 10, Precio: 900, Descuento: 0.10},
 		{Nombre: "Cargador", Cantidad: 2, Precio: 25, Descuento: 0.20},
-		{Nombre: "iphone 16", Cantidad: 5, Precio: 1000, Descuento: 0.15},
+		{Nombre: "iPhone 16", Cantidad: 5, Precio: 1000, Descuento: 0.15},
 	}
 
-	fmt.Println("Aplicando descuentos permanentes...")
+	// 2. Categorizar los productos
+	categorias := make(map[string][]Producto)
 
-	for i := range inventario[:1] {
-		inventario[i].AplicarDescuentoReal()
+	categorias["Telefonos"] = []Producto{inventario[0], inventario[2]}
+	categorias["Accesorios"] = []Producto{inventario[1]}
+
+	fmt.Println("--- Reporte por Categorías ---")
+
+	// 3. Rrecoorer el map de categorías
+	for cat, lista := range categorias {
+		fmt.Printf("\nCategoría: %s\n", cat)
+		fmt.Println("---------------------------")
+
+		for _, p := range lista {
+			// Aplicar descuento solo si es un teléfono
+			if cat == "Telefonos" {
+				p.AplicarDescuentoReal()
+			}
+			fmt.Printf("- %s: $%.2f\n", p.Nombre, p.Precio)
+		}
 	}
 
-	for _, item := range inventario {
-		fmt.Printf("Producto: %-10s | Nuevo Precio: $%.2f\n", item.Nombre, item.Precio)
-	}
-	fmt.Println("________________________________")
-	inventario[2].ActualizarPrecio(850)
-	fmt.Printf("Nuevo precio especial para el iPhone 16 $%.2f\n", inventario[2].Precio)
-
+	// Crear un nuevo slice basado en uno existente
+	ofertasRelampago := inventario[0:2]
+	fmt.Printf("\nTenemos %d ofertas relámpago hoy.\n", len(ofertasRelampago))
 }
