9186a81a74e6085b73a53adce070800ca84976ba
juanes <juannomesque12@gmail.com>
2026-03-05 16:22:30 -0500

    2. running a command and showing output

--- exec-command/main.go
+++ exec-command/main.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"log"
+	"os"
 	"os/exec"
 )
 
 func main() {
 	cmd := exec.Command("ls", "-lah")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
 	err := cmd.Run()
 	if err != nil {
 		log.Fatal(err)
