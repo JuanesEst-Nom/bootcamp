fa34b8a0d23d921762ef137e65a0a0a5080e53e9
juanes <juannomesque12@gmail.com>
2026-03-05 16:21:02 -0500

    1. running a command

--- exec-command/main.go
+++ exec-command/main.go
@@ -1,37 +1,14 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"io"
 	"log"
-	"os"
 	"os/exec"
 )
 
-type CapturingPassThroughWriter struct {
-	buf bytes.Buffer
-	w   io.Writer
-}
-
-func (w *CapturingPassThroughWriter) Write(d []byte) (int, error) {
-	w.buf.Write(d)
-	return w.w.Write(d)
-}
-
 func main() {
-	fmt.Println("Starting command with progress...")
-
 	cmd := exec.Command("ls", "-lah")
-
-	stdoutWriter := &CapturingPassThroughWriter{w: os.Stdout}
-	cmd.Stdout = stdoutWriter
-	cmd.Stderr = os.Stderr
-
 	err := cmd.Run()
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Printf("\n--- Final Summary (from buffer) ---\nSize: %d bytes\n", stdoutWriter.buf.Len())
 }
