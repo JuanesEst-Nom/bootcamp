3a9bfafc5c80707566898c4e7753a6a85ce67755
juanes <juannomesque12@gmail.com>
2026-03-06 09:28:50 -0500

    6. Capture output but also show progress on stdout

--- exec-command/main.go
+++ exec-command/main.go
@@ -3,14 +3,35 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
+	"log"
+	"os"
 	"os/exec"
 )
 
+type CapturingPassThroughWriter struct {
+	buf bytes.Buffer
+	w   io.Writer
+}
+
+func (w *CapturingPassThroughWriter) Write(d []byte) (int, error) {
+	w.buf.Write(d)
+	return w.w.Write(d)
+}
+
 func main() {
-	cmd := exec.Command("ls", "-lah", "non-existent-file")
-	var outb, errb bytes.Buffer
-	cmd.Stdout = &outb
-	cmd.Stderr = &errb
-	_ = cmd.Run()
-	fmt.Printf("Stdout: %s\nStderr: %s\n", outb.String(), errb.String())
+	fmt.Println("Starting command with progress...")
+
+	cmd := exec.Command("ls", "-lah")
+
+	stdoutWriter := &CapturingPassThroughWriter{w: os.Stdout}
+	cmd.Stdout = stdoutWriter
+	cmd.Stderr = os.Stderr
+
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("\n--- Final Summary (from buffer) ---\nSize: %d bytes\n", stdoutWriter.buf.Len())
 }
